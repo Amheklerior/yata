218618aa81f96c972e8b7fbaf6b2b9d430aee30a
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2025-05-10 11:38:08 +0200

    scaffold a react-ts app with vite

---
+++ webapp/src/App.tsx
@@ -0,0 +1,23 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <p>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
