e45d2c7bba7ca664ebe4da8ad8188fb173cba842
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2025-05-10 13:46:35 +0200

    run formatter globally

--- webapp/src/components/Header.tsx
+++ webapp/src/components/Header.tsx
@@ -2,7 +2,7 @@ import type { FC } from "react";
 
 export const Header: FC = () => (
   <header>
-    <div className="wrapper py-12 debug" data-width="wide">
+    <div className="wrapper debug py-12" data-width="wide">
       <h1 className="text-6xl">YATA</h1>
       <h2 className="text-4xl">Yet Another Todo App</h2>
     </div>
--- webapp/src/layouts/Layout.tsx
+++ webapp/src/layouts/Layout.tsx
@@ -3,10 +3,10 @@ import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 
 export const Layout: FC<PropsWithChildren> = ({ children }) => (
-  <div className="flex flex-col text-center h-screen">
+  <div className="flex h-screen flex-col text-center">
     <Header />
     <main className="grow">
-      <div className="wrapper debug py-6 h-full">{children}</div>
+      <div className="wrapper debug h-full py-6">{children}</div>
     </main>
     <Footer />
   </div>
