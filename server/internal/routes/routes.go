54bb62ff43a22cf546bd2d0da1e851fcc94b7f16
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2025-05-10 09:53:11 +0200

    add sensible middlewares

--- server/internal/routes/routes.go
+++ server/internal/routes/routes.go
@@ -1,12 +1,22 @@
 package routes
 
 import (
+	"time"
+
 	"github.com/Amheklerior/yata/server/internal/app"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func SetupRoutes(app *app.Application) *chi.Mux {
 	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
 
 	r.Get("/healthcheck", app.HealthCheck)
 
